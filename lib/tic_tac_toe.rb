3ff86fc83da536f53a89eba48fb9313a60abc594
aarondevon <me@aarondevon.com>
2016-05-16 13:24:18 -0500

    Finish #display_board method

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,18 @@
 
+WIN_COMBINATIONS = [
+  [0, 1, 2], # Top row
+  [3, 4, 5], # Middle row
+  [6, 7, 8], # Bottom row
+  [0, 3, 6], # Vertical left
+  [1, 4, 7], # Vertical middle
+  [2, 5, 8], # Vertical right
+  [0, 4, 8], # Diagonal left
+  [2, 4, 6] # Diagonal right
+]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
