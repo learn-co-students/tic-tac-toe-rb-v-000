dd56a344a6973a3752f8329a1cd7b2c96cf50dbf
William Story <wstory@villanova.edu>
2016-08-25 15:54:19 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,149 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, character)
+  board[index] = character
+end
+
+def position_taken?(board, index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == true
+    false
+  elsif index > 8
+    false
+  elsif index < 0
+    false
+  else
+    true
+  end
+end
+
+def turn(board)
+  loop do
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+
+      if valid_move?(board, index) == true
+          move(board, index, current_player(board))
+          display_board(board)
+        break
+      end
+    end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space != " "
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+def won?(board)
+    WIN_COMBINATIONS.each do |win_combo|
+      win_index_1 = win_combo[0]
+      win_index_2 = win_combo[1]
+      win_index_3 = win_combo[2]
+
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combo
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combo
+      else
+        false
+      end
+    end
+
+    if board.include?("X" || "O") == false
+      false
+    end
+
+end
+
+def full?(board)
+  if board.include?(" ")
+    false
+  else
+    true
+  end
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  draw?(board) || won?(board)
+end
+
+def winner(board)
+  x_count = board.select{|i| i == "X"}
+  o_count = board.select{|i| i == "O"}
+  if x_count.length > o_count.length
+    "X"
+  elsif o_count.length > x_count.length
+    "O"
+  else
+    nil
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  winner = winner(board)
+  if won?(board)
+    puts "Congratulations #{winner}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  else
+    nil
+  end
+end
