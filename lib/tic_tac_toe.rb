e412397bde9bf24bba747b1a87ad8e39f7bbcca2
charlie lee <ctlee9096@gmail.com>
2016-09-08 12:11:07 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -178,3 +178,13 @@ def winner(board)
   end
 end
 #PLAY METHOD###############################
+until the game is over
+  take turns
+end
+
+if the game was won
+  congratulate the winner
+else if the game was a draw
+  tell the players it has been a draw
+end
+############################################333
