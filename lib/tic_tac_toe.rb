b080140a969321ae7d77e60e9a402a7a1ed90aa2
charlie lee <ctlee9096@gmail.com>
2016-09-10 03:04:05 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,4 @@
-#require 'pry'
+require 'pry'
 WIN_COMBINATIONS = [
   [0,1,2], # Top row, #had extra comma&deleted comma, #add comma to delineate element of array WIN_COMBINATIONS
   [3,4,5],  # Middle row, #add comma to delineate element of array WIN_COMBINATIONS
@@ -54,18 +54,6 @@ end
 #else
 #  ask for input again until you get a valid move
 #end
-############################################################################
-puts "Welcome to Tic Tac Toe!"
-puts "Hi! what's your input?"
-user_input = gets.strip
-index = input_to_index (user_input) #missing method input_to_index
-  if valid_move?(board, index)
-    then move(board, index)
-  else
-    puts "Hi! what's your input?"
-  end
-display_board(board) # to call the display_board again with our new updated board
-
 #define turn_count
 #require 'pry'
 def turn_count (board)
@@ -202,6 +190,7 @@ def turn(board)
 end
 #PLAY METHOD###############################
 def play? (board)
+  binding.pry
     if over?(board) == true
       current_player(board)
     elsif over?(board) != true
