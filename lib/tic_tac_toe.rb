1b427c6c7ddf37e09d02d5894037091b9be9ee3e
Shilpa N <piawork1@gmail.com>
2016-10-14 04:16:05 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ def turn(board)
   input=gets.chomp
   index= input_to_index(input)
   if valid_move?(board, index)
-  move(board, index, character="X")
+  move(board, index, current_player(board))
   else
     turn(board)
   end
