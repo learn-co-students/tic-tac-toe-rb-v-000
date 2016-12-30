325cc8a6c5b73e27b90f2bd4cebf1e516c1b1ec3
Elias Ventura <eliasventura23@gmail.com>
2016-12-30 01:30:08 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,20 +1,23 @@
 WIN_COMBINATIONS = [
-  [0,1,2], [3,4,5], [6,7,8],
-  [0,3,6], [1,4,7], [2,5,8],
-  [0,4,8], [2,4,6]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
 ]
 
 def play(board)
   while !over?(board)
-  turn(board)
-end
-if won?(board)
-
-puts "Congratulations #{winner(board)}!"
-elsif draw?(board)
-  puts "Cats Game!"
-end
-
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
 end
 
 def display_board(board)
@@ -25,49 +28,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  index = user_input.to_i-1
-end
-
-def move(board, index, char)
-  board[index] = char
-end
-
-def position_taken?(board, index)
-  !(board[index] == " " || board[index] == nil)
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def current_player(board)
-  if turn_count(board) % 2 == 0
-    "X"
-  else turn_count(board) % 2 == 1
-    "O"
-  end
-end
-
-def turn_count(board)
-  counter = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-  counter += 1
-end
-end
-counter
-end
-
-def turn(board)
-puts "Please enter 1-9:"
-input = gets.strip
-index = input_to_index(input)
-if valid_move?(board, index)
-  move(board, index, current_player(board))
-  display_board(board)
-else
-  turn(board)
+def valid_move?(board, position)
+  position.between?(0,8) && !position_taken?(board, position)
 end
 
 def won?(board)
@@ -75,11 +37,11 @@ def won?(board)
     board[combo[0]] == board[combo[1]] &&
     board[combo[1]] == board[combo[2]] &&
     position_taken?(board, combo[0])
-end
+  end
 end
 
 def full?(board)
-  board.all? {|token| token == "X" || token == "O"}
+  board.all?{|token| token == "X" || token == "O"}
 end
 
 def draw?(board)
@@ -90,9 +52,42 @@ def over?(board)
   won?(board) || draw?(board)
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  position = input_to_index(user_input)
+  if valid_move?(board, position)
+    move(board, position, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, position)
+  board[position]== "X" || board[position] == "O"
+  # Creates a stop on RSpec
+  # !(board[location].nil? || board[location] == "")
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def move(board, position, player)
+  board[position] = player
+end
+
 def winner(board)
   if winning_combo = won?(board)
     board[winning_combo.first]
-  end
   end
 end
