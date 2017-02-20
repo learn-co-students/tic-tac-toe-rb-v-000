ed77ae3fd3166385bf0fa204be1f432c3140f3ba
Sarah Bly <sbly@caliberbetaacademy.org>
2017-02-20 00:13:27 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,111 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, value)
+  board[index] = value
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    puts "Please enter 1-9:"
+    input = gets.strip
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |winning_combination|
+    position_1 = board[winning_combination[0]]
+    position_2 = board[winning_combination[1]]
+    position_3 = board[winning_combination[2]]
+    position_1 == position_2 && position_2 == position_3 && position_taken?(board, winning_combination[0])
+  end
+end
+
+def full?(board)
+  board.all? do |token|
+    token == "X" || token == "O"
+  end
+end
+
+def draw?(board)
+  won?(board) == nil && full?(board) == true
+
+end
+
+def over?(board)
+  won?(board) != nil || draw?(board)
+end
+
+def winner(board)
+  if won?(board) != nil
+    board[(won?(board)[0])]
+  else
+    nil
+  end
+end
+
+def play(board)
+  while over?(board) == false
+    turn(board)
+    won?(board)
+  end
+  if won?(board) != nil
+    puts "Congratulations #{winner(board)}!"
+  end
+  if draw?(board)
+    puts "Cats Game!"
+  end
+
+end
