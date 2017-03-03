62256f318ce351f292570366dc52e0cc31916beb
Rachel Walwood <walwoodr@gmail.com>
2017-03-03 18:59:06 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,7 +63,7 @@ def turn(board)
       input = gets.strip
       index = input_to_index(input)
       if valid_move?(board, index)
-        move(board, index, char = "X")
+        move(board, index, player_char)
         display_board(board)
         break
       end
