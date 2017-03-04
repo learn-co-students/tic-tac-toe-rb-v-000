f94623d9c9a487f35e9c036145d67678d0be5592
Rebecca Bratu <beckybratu@gmail.com>
2017-03-04 06:08:03 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, token = current_player) 
+def move(board, index, token) 
   board[index] = token  
 end
 
@@ -40,7 +40,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, token = current_player)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
