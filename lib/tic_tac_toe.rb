a38a14f4cdd2f8e20bab18be9bae4e0ce0d5e624
Kristy Peterson <kpeterson.kap@gmail.com>
2017-03-07 06:19:25 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,9 +45,9 @@ def turn(board)
   index = input_to_index(input)
     if valid_move?(board, index)
       move(board, index, char=current_player(board))
-      #display_board(board)
-    #else
-    #  turn(board)
+      display_board(board)
+    else
+      turn(board)
     end
 end
 
