a71b1e1567d5740b60927c2f4f6348def982f4bb
Julia Thompson <thompson.julia@gmail.com>
2017-03-11 20:52:06 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,101 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+  separator = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts separator
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts separator
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board,index,player_token)
+  board[index] = player_token
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board,index)
+  if index.between?(0,8) && !position_taken?(board,index)
+    true
+  else
+    false
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board,index)
+    move(board,index,player_token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  turns = 0
+
+  board.each do |position|
+    if position == "X" || position == "O"
+      turns += 1
+    end
+  end
+  turns
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+    board[win_combination[0]] == board[win_combination[1]] &&
+    board[win_combination[1]] == board[win_combination[2]] &&
+    position_taken?(board, win_combination[0])
+  end
+end
+
+def full?(board)
+  board.all? {|position| position == "X" || position == "O"}
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  end
+end
+
+def over?(board)
+  full?(board) || won?(board)
+end
+
+def winner(board)
+  if won?(board)
+    winning_combination = won?(board)
+    return board[winning_combination[0]]
+  else
+    nil
+  end
+end
