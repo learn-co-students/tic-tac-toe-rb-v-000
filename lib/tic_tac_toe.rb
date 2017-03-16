f849a6e7981b9d6583415614fd7bc2868cf2e7ef
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-03-16 17:23:34 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,3 @@
-#defines a constant WIN_COMBINATIONS with arrays for each win combination
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -10,7 +9,6 @@ WIN_COMBINATIONS = [
   [6,4,2]
 ]
 
-#prints arbitrary arrangements of the board
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -19,17 +17,18 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#converts a user_input to an integer, subtracts 1 from the user_input, returns -1 for strings without integers
 def input_to_index(user_input)
     user_input.to_i - 1
 end
 
-#does not allow for a default third argument, takes three arguments: board, position, and player token, allows "X" player in the bottom right and "O" in the top left
 def move(board, index, player)
     board[index] = player
 end
 
-#returns true/false based on position in board
+def move (index, current_player = "X")
+  board[index] = current_player
+end
+
 def position_taken?(board,position)
   if board[position] == " "
     false
@@ -39,11 +38,9 @@ def position_taken?(board,position)
     false
     else   board[position] == "X" || "O"
     true
-
   end
 end
 
-#returns true/false based on position
 def valid_move?(board, index)
    if position_taken?(board, index)
      false
@@ -52,4 +49,69 @@ def valid_move?(board, index)
    else
      nil
    end
- end
+end
+
+def move(board, index, value= "X")
+   board[index]=value
+end
+
+def turn (board)
+   puts "Please enter 1-9:"
+     input=gets.strip
+     index = input_to_index(user_input) #change from input to user_input
+       if valid_move?(board, index)
+           move(board, index, current_player(board)) # I add current_player(board)
+           display_board(board)
+       else
+          turn(board)
+       end
+end
+
+
+def move (board, index, player)
+  board[index] =  player # I removed "X"  and change current_player to player
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |turn_count_board|
+    if turn_count_board == "X" || turn_count_board == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board) #Replaced opposite from  full?(board) && !won?(board) to !won?(board)....
+end
+
+def over?(board)
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
