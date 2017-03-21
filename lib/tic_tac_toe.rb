eef5974b26b4a42b3edfc4e7dbed863fd47f5df5
Tavish Harrison <creationmargin26@gmail.com>
2017-03-21 05:10:40 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,103 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def input_to_index(index)
+    index.to_i - 1
+end
+
+def move(board, index, input)
+  board[index] = input
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+  elsif board[index] == "X" || "O"
+    false
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+    if !valid_move?(board, index)
+      turn(board)
+    end
+    move(board, index, current_player(board))
+    display_board(board)
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def current_player(board)
+	turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [0, 4, 8]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |wincombo|
+    if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X"
+      return wincombo
+    elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O"
+      return wincombo
+    end
+  end
+  false
+end
+
+def full?(board)
+  board.all?{|full| full == "X" || full == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |wincombo|
+    if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X"
+      return "X"
+    elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O"
+      return "O"
+    end
+  end
+  nil
+end
+
+def play(board)
+  current_player(board)
+  until over?(board)
+    turn(board)
+  end
+    if won?(board)
+      puts "Congratulations X!"
+      puts "Congratulations O!"
+    end
+    puts "Cats Game!" if draw?(board)
+end
