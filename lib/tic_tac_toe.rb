d07840ade01e9113301be0cf937e673e711050cd
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-03-22 21:59:25 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,16 +34,18 @@ end
 =end
 
 def position_taken?(board,position)
+
   if board[position] == " "
     false
     #elsif board[position] == ""
     #false
     elsif  board[position] == nil
-    #false
-    else   board[position] == "X" || "O"
+    false
+   else   board[position] == "X" || "O"
     true
   end
 end
+
 
 def valid_move?(board, index)
    if position_taken?(board, index)
