836ae5fa5c43e6e5a352a0bad369190df8eeb6e5
Nicole Therien <nicoletherien@yahoo.com>
2017-04-04 20:04:48 +0000

    Done.

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../lib/tic_tac_toe.rb'
 
-describe './lib/tic_tac_toe.rb' do  
+describe './lib/tic_tac_toe.rb' do
   describe '#play' do
     it 'asks for players input on a turn of the game' do
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       allow($stdout).to receive(:puts)
       allow(self).to receive(:over?).and_return(false, true)
 
-      expect(self).to receive(:gets).at_least(:once).and_return("1")
 
+      expect(self).to receive(:gets).at_least(:once).and_return("1")
       play(board)
     end
 
@@ -30,6 +30,7 @@ describe './lib/tic_tac_toe.rb' do
       allow(self).to receive(:over?).and_return(false, true)
 
       play(board)
+
       expect(board).to match_array(["X", " ", " ", " ", " ", " ", " ", " ", " "])
     end
 
