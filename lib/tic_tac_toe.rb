57dc095ee17351f181722c34ae67aac7421750b4
Brian Zoebisch <zoebisch@gmail.com>
2017-04-14 20:46:42 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,59 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    wins_x = []
+    wins_o = []
+    combo.each do |check|
+      if board[check]== "X"
+        wins_x << check
+        if wins_x.count == 3
+          return wins_x
+        end
+      elsif board[check] == "O"
+        wins_o << check
+        if wins_o.count == 3
+          return wins_o
+        end
+      end
+    end
+  end
+  return false
+end
+
+def draw?(board)
+  return full?(board) && !won?(board)
+end
+
+def full?(board)  #I believe the following methods can be collapsed using yield, but I don't think the tests will pass because we must define unique method names for the tests
+  return board.all?{ |i| i.is_a?(String) && i != " "}
+end
+
+def draw?(board)
+  return full?(board) && !won?(board)
+end
+
+def over?(board)
+  return draw?(board) || won?(board)
+end
+
+def winner(board)
+  if won?(board) && board.count{|x| x.upcase == "X"} > board.count{|o| o.upcase == "O"}
+    return "X"
+  elsif won?(board) && board.count{|x| x.upcase == "X"} < board.count{|o| o.upcase == "O"}
+    return "O"
+  end
+  return nil
+end
