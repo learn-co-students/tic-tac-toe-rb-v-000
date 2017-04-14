2bc95ce1fa8623630fc8c569905300d589e2e288
Brian Zoebisch <zoebisch@gmail.com>
2017-04-14 20:50:40 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,6 +6,31 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
@@ -31,10 +56,6 @@ def won?(board)
     end
   end
   return false
-end
-
-def draw?(board)
-  return full?(board) && !won?(board)
 end
 
 def full?(board)  #I believe the following methods can be collapsed using yield, but I don't think the tests will pass because we must define unique method names for the tests
