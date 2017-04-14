b7630b2080705484377a0fc3a9898a065c8d06fa
Brian Zoebisch <zoebisch@gmail.com>
2017-04-14 22:30:01 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,16 +6,53 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def play(board)
+  while turn_count(board) < 2 #Can't win until at least 3 moves, so don't check yet
+    turn(board)
+  end
+  while !over?(board)
+    turn(board)
+      if won?(board)
+        return "#{winner?(board)} wins!"
+      elsif full?(board) || draw?(board)
+        return "It's a draw!"
+      end
+  end
+end
+
 def turn(board)
-  
+
+  player_token = current_player(board)
+  if player_token == nil
+    puts "Please select X or O"
+    player_token = gets.strip.upcase
+  end
+
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, current_player)
+    move(board, index, player_token)
     display_board(board)
+  end
+end
+
+def turn_count(board)
+  return board.count{|occupied| occupied != " "}
+end
+
+def current_player(board)
+  if turn_count(board) == 0 #Define the first player, X or O by user input
+    board.each do |find_x_or_o|
+      if board[find_x_or_o] != " "
+        first_move = board[find_x_or_o]
+        puts "the first move is #{board[find_x_or_o]}"
+      end
+    end
+  elsif turn_count(board) % 2 == 0 && first_move == "X"
+    return "X"
   else
-    turn(board)
+    return "O"
   end
 end
 
