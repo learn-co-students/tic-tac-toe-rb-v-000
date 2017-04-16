c9df7ccc590de2945751de65eb3509d78a16bf7b
Brian Zoebisch <zoebisch@gmail.com>
2017-04-16 09:11:57 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,51 +7,37 @@ def display_board(board)
 end
 
 def play(board)
-
   while !over?(board)
     turn(board)
   end
-
   if won?(board)
     puts "Congratulations #{winner(board)}!"
   else
     puts "Cats Game!"
   end
-
 end
 
 def turn(board)
-
   turn_count(board) == 0 ? player_token = "X" : player_token = current_player(board)
-
   puts "Player #{player_token}, it is your turn."
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-
-  if valid_move?(board, index)
-    move(board, index, player_token)
-    display_board(board)
-  else
+  if !valid_move?(board, index)
     puts "#{input} is not a possible, pick another space!"
     turn(board)
+  #else
   end
+  move(board, index, player_token)
+  display_board(board)
 end
 
 def turn_count(board)
    board.count{|occupied| occupied != " "}
 end
 
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
 def input_to_index(user_input)
   user_input.to_i - 1
-end
-
-def move(board, index, current_player)
-  board[index] = current_player
 end
 
 def valid_move?(board, index)
@@ -62,27 +48,30 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-
-def won?(board)
-  WIN_COMBINATIONS.each do |combo|
-    if combo.all? {|id| board[id] == "X"} || combo.all? {|id| board[id] == "O"}
-      return true
-    end
-  end
-  return nil
+def move(board, index, current_player)
+  board[index] = current_player
 end
 
-def full?(board)
-   !board.any?{|i| i == " "}
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
 def draw?(board)
-   full?(board) && !won?(board)
+  !won?(board) && full?(board)
+end
+
+def full?(board)
+  !board.any?{|i| i == " "}
 end
 
 def over?(board)
-   draw?(board) || won?(board)
+  draw?(board) || won?(board)
+end
+
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+def won?(board)
+  winner(board) == "X" || winner(board) == "O" ? true : nil
 end
 
 def winner(board)
