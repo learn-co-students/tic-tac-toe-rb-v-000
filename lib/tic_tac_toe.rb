9abac39e014635e19e578ad8dc40dda6e118e8eb
Ary Baldioceda <abaldioceda@gmail.com>
2017-05-02 17:22:55 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,42 @@
+require "pry"
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8],
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8],[6,4,2]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(number)
+  number.to_i - 1
+end
+
+def move(array, index, character)
+  array[index] = character
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def turn
+  puts "Please enter 1-9:"
+  input == gets.strip
+  input == input_to_index(input)
+  if valid_move?(board, input)
+    move(board, input, character)
+    display_board(board)
+  else
+    turn
+  end
+end
