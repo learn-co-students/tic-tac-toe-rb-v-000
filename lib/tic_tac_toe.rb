a3772d43e652a6b0dbfecb940dede9fa887a0b57
Christina <github email address>
2017-05-09 21:40:56 -0600

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,8 +31,8 @@ def input_to_index(user_input)
 end
 
 #Move
-def move(board, index, input)
-  board[index] = input
+def move(board, index, piece)
+  board[index] = piece
 end
 
 #Position Taken?
@@ -57,15 +57,99 @@ def turn(board)
     input = gets.strip
     index = input_to_index(input)
   if valid_move?(board, index) == true
-    move(board, index, input)
-    display_board(board[index])
+    move(board, index, current_player(board))
+    display_board(board)
   else
     puts "Invalid entry. Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
     until valid_move?(board, index) == true
-      input = gets.strip
-      index = input_to_index(input)
-      move(board, index, input)
-      display_board(board[index])
+      move(board, index, current_player(board))
+      display_board(board)
     end
+  end
+end
+
+#Turn count
+def turn_count(board)
+  count = 0
+  board.each do |index|
+    if index != " "
+      count += 1
+    end
+  end
+  count
+end
+
+#Current player
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+#won
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+  if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+    return win_combo
+  elsif (board[win_combo[0]])  == "O" && (board[win_combo[1]])  == "O" && (board[win_combo[2]]) == "O"
+    return win_combo
+  end
+end
+ false
+end
+
+#full
+def full?(board)
+  board.none? do |i|
+    i == " " || i.nil?
+  end
+end
+
+#Draw
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+#Over
+def over?(board)
+  full?(board)
+end
+
+#Winner
+def winner(board)
+  if draw?(board) || !full?(board) && !won?(board)
+    return nil
+  elsif (board[won?(board)[0]] == "X")
+    return "X"
+  else (board[won?(board)[0]] == "O")
+    return "O"
+  end
+end
+
+#Play
+player_1 = "X"
+player_2 = "O"
+def play(board, current_player)
+  if player_turn == player_1
+    puts "Please enter your turn X."
+    input = gets.chomp
+    while !over?(board) && !won?(board)
+      turn
+    end
+  else player_turn == player_2
+    puts "Please enter your turn O."
+    input = gets.chomp
+    while !over?(board) && !won?(board)
+      turn
+    end
+  end
+  if won?(board)
+    "Congratulations! You are the winner #{winner(board)}!"
+  else draw?(board)
+    "You both win! Not really, it's a cat's game."
   end
 end
