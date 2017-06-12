4fabbeec8a709399b89eddb33f1e048b9f6be284
Sarah Schaller <sarcsch@aol.com>
2017-06-12 17:38:24 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,16 +1,14 @@
-# rspec spec/01_tic_tac_toe_spec.rb
 
-WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
+WIN_COMBINATIONS =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ]
-
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -20,23 +18,20 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
 def input_to_index(input)
-  input.to_i
-  index = input.to_i - 1
+   input.to_i
+   index = input.to_i - 1
 end
-
 
 def move(board, index, char)
   board[index] = char
-  return board
+  turn_count(board)
+  display_board(board)
 end
-
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
-
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
@@ -49,43 +44,32 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index, current_player(board))
-    display_board(board)
   else
     turn(board)
   end
 end
 
-
 def turn_count(board)
   counter = 0
   board.each do |element|
     if element == "X" || element == "O"
-    counter += 1
+      counter += 1
     end
   end
   return counter
 end
 
-
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
-
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-      return win_combination
-    elsif
-      board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
-      return win_combination
-    end
+  WIN_COMBINATIONS.detect do |win_combination|
+    board[win_combination[0]] == board[win_combination[1]] &&
+    board[win_combination[1]] == board[win_combination[2]] &&
+    position_taken?(board, win_combination[0])
   end
-  else
-    return false
 end
-
-
 
 def full?(board)
   board.all? do |token|
@@ -93,47 +77,27 @@ def full?(board)
   end
 end
 
-
-
 def draw?(board)
-  full?(board) && !won?(board)
+  won?(board) == nil && full?(board) == true
 end
-
 
 def over?(board)
-  if won?(board) == true || draw?(board) == true || full?(board) == true
-    return true
-  end
-else
-  return false
+  draw?(board) == true || won?(board) != nil
 end
-
 
 def winner(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-      return "X"
-    elsif
-      board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
-      return "O"
-    end
+  if won?(board) != nil
+    winner = board[won?(board)[0]]
   end
-    else
-      return nil
 end
-
-
-# rspec spec/02_play_spec.rb
 
 def play(board)
   until over?(board) == true
     turn(board)
   end
-  if winner(board) == "X"
-    return "Congratulations X!"
-  elsif winner(board) == "O"
-    return "Congratulations O!"
-  else
-    return "Cats Game!"
-  end
+  if draw?(board) == true
+       puts "Cats Game!"
+  else won?(board)
+     puts "Congratulations #{winner(board)}!"
+   end
 end
