1bd82bb4c4cee48c56d6f1b3c7a1ef4a8012297c
Adam Ping <thepingster@gmail.com>
2017-06-19 15:12:16 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, character = "X")
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
