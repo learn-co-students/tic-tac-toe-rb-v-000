8e504689e8f51c6b3e4efd184a46d82edc4c7b44
Donna Hogan <donnamcla@gmail.com>
2017-07-02 19:56:42 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && position_taken?(board, index) == false
+  if index.between?(0, 8) && !position_taken?(board, index)
     return true
   else
     return false
@@ -48,7 +48,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index) == true
+  if valid_move?(board, index)
     move(board, index, value="X")
   else
     puts "Please try again"
