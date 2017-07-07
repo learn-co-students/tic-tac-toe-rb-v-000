9de112f471e78d41398d837307dd49577b794203
Jerry Boyce <jokerdelta@yahoo.com>
2017-07-07 15:27:24 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,10 +22,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-
-  board[index] = current_player # THIS IS THE PROBLEM!!
-
+def move(board, index,current_player = "X") # problem is with current_player method
+  board[index] = current_player
 end
 
 def position_taken?(board, location)
