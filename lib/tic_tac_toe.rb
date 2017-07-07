b138dda9d443e0112c16b9059aa4647e4aa404ed
Jerry Boyce <jokerdelta@yahoo.com>
2017-07-07 14:55:26 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,9 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player = "X")
-  board[index] = current_player
+
+  board[index] = current_player # THIS IS THE PROBLEM!!
+
 end
 
 def position_taken?(board, location)
