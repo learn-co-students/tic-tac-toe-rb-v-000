d024910c18d9943fabb3b1cacdb9d14c746c530d
Kevin Kim <kevikim33@gmail.com>
2017-08-04 20:49:47 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -101,16 +101,14 @@ def play(board)
   end
 end
 
-=begin
-
-until the game is over
-  take turns
-end
-
-if the game was won
-  congratulate the winner
-else if the game was a draw
-  tell the players it has been a draw
-end
-
-=end
+## solution method--always cleaner than mine.
+# def play(board)
+#   while !over?(board)
+#     turn(board)
+#   end
+#   if won?(board)
+#     puts "Congratulations #{winner(board)}!"
+#   elsif draw?(board)
+#     puts "Cats Game!"
+#   end
+# end
