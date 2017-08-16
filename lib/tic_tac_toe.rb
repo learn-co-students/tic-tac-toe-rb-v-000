80a9930f8ab7d0c6a527e408270815bd492874e3
Harvie Brown <kurohyo70@gmail.com>
2017-08-16 15:40:45 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,6 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-  #binding.pry
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, current_player(board))
