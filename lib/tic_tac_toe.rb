fa3b93c69a57897203fe23cd3ec48fa5562995a9
Justin Daniel <justinddaniel@gmail.com>
2017-09-11 18:25:40 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,109 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if (!position_taken?(board, index) && index.between?(0, 8))
+    true
+  end
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def move(board, index, token)
+    board[index] = token
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  token = current_player(board)
+  if valid_move?(board, index)
+    move(board, index, token)
+  else
+    turn(board)
+  end
+  display_board(board)
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  t = turn_count(board)
+  if t % 2 == 0
+    return "X"
+  else return "O"
+  end
+end
+
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |winc|
+    board[winc[0]] == board[winc[1]] &&
+    board[winc[1]] == board[winc[2]] &&
+    position_taken?(board, winc[0])
+  end
+end
+
+def full?(board)
+  board.all? do |token|
+    token == "X" || token == "O"
+  end
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  if winc = won?(board)
+    board[winc.first]
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
