b73c364d94e8f0b62e74dfc3ef9a183d55d29864
Richard Hanolt <rhanolt@gmail.com>
2017-09-17 01:15:00 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,103 @@
 
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  elsif turn_count(board).odd?
+    "O"
+  end
+end
+def move(board, user_input, player_token)
+  board[user_input] = player_token
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |winner| #[0, 1, 2]
+      board[winner[0]] == board[winner[1]] && board[winner[0]] == board[winner[2]] && position_taken?(board, winner[0])
+  end
+end
+
+def full?(board)
+  board.all? do |board_position|
+    board_position == "X" || board_position == "O"
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  draw?(board) || (won?(board) && full?(board)) || (won?(board) && !full?(board))
+end
+
+def winner(board)
+  if won?(board)
+    board[won?(board)[0]]
+  end
+end
+
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0,8)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |occupied|
+    if occupied == "X" || occupied == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if draw?(board)
+    puts "Cat's Game!"
+  elsif won?(board)
+    puts "Congratulations #{winner(board)}!"
+  end
+end
