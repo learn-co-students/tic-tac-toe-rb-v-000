6cb8a8677cc167c153faff80cc79a789ceefb1f8
Thomas Sharkey <tbsharkey@gmail.com>
2017-10-09 17:49:24 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,17 +31,6 @@ def turn(board)
   end
 end
 
-def play(board)
-  counter = 0
-loop do
-  counter += 1
-    turn(board)
-if counter >= 9
-  break
-  end
-end
-end
-
 def turn_count(board)
     counter = 0
     board.each do |arrayCell|
