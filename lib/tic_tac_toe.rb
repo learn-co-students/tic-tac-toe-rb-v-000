aa1b9395dad495dc1123435e727a9fc4310f8da2
Eric Johnson <0216johnsoneric@gmail.com>
2017-11-04 23:35:07 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,117 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(player_input)
+  player_input.to_i - 1
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  turn_count(board)
+  if  turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_comb|
+    win_index_1 = win_comb[0]
+    win_index_2 = win_comb[1]
+    win_index_3 = win_comb[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    position_1 == position_2 && position_2 == position_3 && position_1 != " "
+  end
+end
+
+def full?(board)
+  theboard = board.select do |check|
+    check == "X" || check == "O"
+  end
+    theboard.size == board.size
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    return false
+  end
+end
+#
+def over?(board)
+  if draw?(board) || won?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    win_check = won?(board)
+    return board[win_check[0]]
+  else
+    return nil
+  end
+end
+
+def play(board)
+  counter = 0
+	until counter == 9
+  counter += 1
+  turn(board)
+  end
+end
