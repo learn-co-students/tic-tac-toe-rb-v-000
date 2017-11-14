68e557eef0ef250fb132fb76f36f047ff38babfd
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-14 23:02:32 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,7 +24,7 @@ def move(board, index, token)
 end
 
 def position_taken?(board, index)
-  board[index] == "X" || board[index] == "O"
+  ["X","O"].include?board[index]
 end
 
 def valid_move?(board, index)
