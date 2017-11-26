f70bc7a17ef76563c362c91a26720b946abf6540
Christopher DiStasio <cadistasio@gmail.com>
2017-11-26 11:08:09 -0600

    Update tic_tac_toe.rb

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,18 +1,3 @@
-
-
-
-# until the game is over
-#   take turns
-# end
-#
-# if the game was won
-#   congratulate the winner
-# else if the game was a draw
-#   tell the players it has been a draw
-# end
-
-
-
 def play(board)
   until over?(board) || won?(board)
     turn(board)
