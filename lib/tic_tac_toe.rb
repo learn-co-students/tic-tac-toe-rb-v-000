088428038b74abfc808205199f2f4c5378235a30
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-10 06:43:54 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,16 @@
 # Helper Method
+board = [" "," "," "," "," "," "," "," "," "]
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
+end
+
+def display_board(board)
+  puts" #{board[0]} | #{board[1]} | #{board[2]} "
+  puts"-----------"
+  puts" #{board[3]} | #{board[4]} | #{board[5]} "
+  puts"-----------"
+  puts" #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # Define your WIN_COMBINATIONS constant
@@ -15,113 +25,175 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
   ]
-  
-WIN_NAMES = [
-  
-  top_row_win = WIN_COMBINATIONS[0],
-  mid_row_win = WIN_COMBINATIONS[1],
-  bottom_row_win = WIN_COMBINATIONS[2],
-  left_vert_win = WIN_COMBINATIONS[3],
-  mid_vert_win = WIN_COMBINATIONS[4],
-  right_vert_win = WIN_COMBINATIONS[5],
-  left_diag_win = WIN_COMBINATIONS[6],
-  right_diag_win = WIN_COMBINATIONS[7]
-  ]
-  
+
+def input_to_index(input)
+  input = input.to_i
+  index = input - 1
+  return index
+end
+
+def position_taken(board, index)
+  if board[index] == " "
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if (index > -1 && index < 9) && !position_taken?(board, index)
+    return true
+  else
+    return false
+  end
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if space != " "
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if count.odd?
+    return "O"
+  end
+  if count.even?
+    return "X"
+  end
+end
+
+
+def turn(board)
+  valid = false
+  while valid == false
+    puts"Please enter 1-9:"
+    input=gets.strip
+    index = input_to_index(input)
+    valid = valid_move?(board, index)
+  end
+  player = current_player(board)
+  move(board, index, player)
+end
+
 def won?(board)
-won = false 
-winner = nil 
-empty = true 
-winning_combo = nil 
+won = false
+winner = nil
+empty = true
+winning_combo = nil
 x_win = false
-o_win = false 
+o_win = false
 
   WIN_COMBINATIONS.each do |combo|
-    
+
       x_win = combo.all? do |space|
         board[space] == "X"
-      end 
-      
+      end
+
       o_win = combo.all? do |space|
         board[space] == "O"
-      end 
+      end
       if x_win || o_win
-        winning_combo = combo 
-      end 
-    
-  end 
+        winning_combo = combo
+      end
 
- 
+  end
+
+
 
 empty = WIN_COMBINATIONS.each do |combo|
   combo.each do |space|
     board[space] == " "
-  end 
-end 
+  end
+end
 
 if winning_combo != nil
   return winning_combo
-end 
+end
 if empty
-  return false 
-end 
+  return false
+end
 if winning_combo == nil && !empty
-  return false 
-end 
+  return false
+end
 
-end 
+end
 
 def winner(board)
   x_win = false
-  o_win = false 
+  o_win = false
 
   WIN_COMBINATIONS.each do |combo|
     x_win = combo.each do |space|
       board[space] == "X"
     end
-  
+
     o_win = combo.each do |space|
       board[space] == "O"
     end
-  end 
+  end
 
   if o_win
     value = "O"
   end
-  if x_win 
+  if x_win
     value = "X"
   end
-   
-end 
+
+end
 
 def full?(board)
   full = board.all? do |space|
     space != " "
-  end 
-  return full 
+  end
+  return full
 end
 
 def over?(board)
   if full?(board) || won?(board)
-    return true 
-  end 
-end 
+    return true
+  end
+end
 
 def draw?(board)
   won = won?(board)
   over = over?(board)
   if !won && over
-    return true 
-  end 
-end 
+    return true
+  end
+end
 
 def winner(board)
   value = won?(board)
   if !value
     return nil
-  else 
+  else
     return board[value[0]]
   end
-end 
-   
-  
+end
+
+def play(board)
+  until won?(board) || over?(board)
+    turn(board)
+  end
+  if draw?(board)
+    puts "Cat's Game!"
+  end
+  if won?(board)
+    if winner(board) == "X"
+      puts "Congratulations X!"
+    end
+    if winner(board) == "O"
+      puts "Congratulations O!"
+    end
+  end
+end
