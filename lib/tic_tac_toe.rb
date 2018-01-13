4a5177e6a812106f42c12d7a09cb578e6fdd9105
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-01-13 16:43:37 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,119 @@
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1 
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+ if index.between?(0,8) && !position_taken?(board, index)
+   true
+ else
+   false
+ end 
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.chomp 
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else turn(board)
+  end
+end 
+
+def turn_count(board)
+ board.select { |index| index == "X" or index == "O"}.length 
+ end 
+
+def current_player(board)
+ if turn_count(board) % 2 == 0 
+   return "X"
+ else
+   return "O"
+ end 
+end 
+
+def won?(board)
+  WIN_COMBINATIONS.find do |wc|
+    if board[wc[0]] == "X" && 
+       board[wc[1]] == "X" && 
+       board[wc[2]] == "X"
+       return wc
+    elsif board[wc[0]] == "O" && 
+          board[wc[1]] == "O" && 
+          board[wc[2]] == "O"
+        return wc
+      else 
+      false 
+    end
+  end
+end
+
+def full?(board)
+  !board.include?(" ")
+end 
+
+def draw?(board)
+  if won?(board)
+    false
+  elsif full?(board)
+    true 
+  else 
+    false 
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  else
+    false
+  end 
+end
+
+def winner(board)
+if won?(board)
+  return board[won?(board)[0]]
+else 
+  nil  
+end 
+end
+
+def play(board)
+  until over?(board)
+  turn(board)
+end
+
+if won?(board)
+  puts "Congratulations #{winner(board)}!"
+elsif draw?(board)
+  puts "Cat's Game!"
+end
+end
