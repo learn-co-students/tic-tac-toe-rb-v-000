c2a361dffab375ee0e521be2470456224e78b3b2
Virginia Herron <inadream@gmail.com>
2018-01-23 04:44:05 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,109 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+# Define your play method below
+def play(board)
+  turns = 0
+  until turns == 9
+    turn(board)
+    turns += 1
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win|
+    board[win[0]] == board[win[1]] && board[win[1]] == board[win[2]] && position_taken?(board, win[0])
+  end
+end
+
+def full?(board)
+  board.all? do |i|
+    i == "X" || i == "O"
+  end
+end
+
+def draw?(board)
+  if won?(board)
+    false
+  elsif full?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    board[won?(board).first]
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |plays|
+    if plays == "X" || plays == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
+end
