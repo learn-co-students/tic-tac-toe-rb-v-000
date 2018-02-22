feb88642c4a665276bb2c8e06b4a65f399fd4a06
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-22 20:26:32 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,154 @@
 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(array, index, value)
+  array[index] = value
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    true
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, value = current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+      count +=1
+    end
+end
+count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  new_arr = []
+  WIN_COMBINATIONS.each do |array|
+
+      win_index_1 = array[0]
+      win_index_2 = array[1]
+      win_index_3 = array[2]
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        new_arr.push(array[0])
+        new_arr.push(array[1])
+        new_arr.push(array[2])
+        return new_arr
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        new_arr.push(array[0])
+        new_arr.push(array[1])
+        new_arr.push(array[2])
+        return new_arr
+
+      end
+    end
+      false
+  end
+
+def full?(board)
+  board.none?{|i| i == " "}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+won?(board)
+  WIN_COMBINATIONS.each do |array|
+
+      win_index_1 = array[0]
+      win_index_2 = array[1]
+      win_index_3 = array[2]
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return "X"
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return "O"
+
+      end
+    end
+      nil
+  end
+
+def play(board)
+  won?(board)
+  turn(board)
+  over?(board)
+
+  while !over?(board) do
+    turn(board)
+    winner = won?(board)
+    if winner
+      print "Congratulations, the winner is #{winner(board)}!"
+      break
+    end
+    if draw?(board)
+      print "Cats Game!"
+      break
+    end
+
+  end
+
+end
