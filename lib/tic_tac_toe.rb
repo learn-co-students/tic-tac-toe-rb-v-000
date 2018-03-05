b9ce21ee91e696a6e09f06b13bb3c9088b8e59d4
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-05 15:05:29 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,3 +95,16 @@ def winner(board)
   end
 end
 
+def play(board)
+ while !over?(board)
+  turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+  puts "Cat's Game!"
+  end
+end
+
+
+  
