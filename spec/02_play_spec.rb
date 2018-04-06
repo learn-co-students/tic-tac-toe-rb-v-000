0f37dc6be23573e417e658daaf20d38fd15876e7
Carrie Savinell <carrielane89@gmail.com>
2018-04-06 14:23:25 +0000

    Done.

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -38,7 +38,7 @@ describe './lib/tic_tac_toe.rb' do
       num_of_turns = 0
       allow($stdout).to receive(:puts)
       allow(self).to receive(:gets).and_return("1","2","3")
-      allow(self).to receive(:over?).and_return(num_of_turns = 0)
+      allow(self).to receive(:over?).and_return(false, false, false, true)
       allow(self).to receive(:turn) do
         num_of_turns += 1
         Process.exit!(true) if num_of_turns > 10
