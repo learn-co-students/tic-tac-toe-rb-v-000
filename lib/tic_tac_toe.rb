4deb471635bd7e0462632a9d16ea2052b0bb2fb4
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-04-17 03:02:42 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,109 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  board[index]== "X" || board[index] == "O"
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    true
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    player_token = current_player(board)
+    move(board, index, player_token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  board.count do |position|
+    position == "X" || position == "O"
+  end
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def play(board)
+  if over?(board)
+    display_board(board)
+  end
+  turn(board)
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token =="O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+
+def winner(board)
+  if combo = won?(board)
+    board[combo.first]
+  end
+end
+
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
