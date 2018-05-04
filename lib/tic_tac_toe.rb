b2ebbf15db8dd67c56d4aab72b563568f5f5f336
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-04 14:34:29 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -137,7 +137,7 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    winner?(board) == "X" || winner?(board) == "O"
+    winner(board) == "X" || winner(board) == "O"
     puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
