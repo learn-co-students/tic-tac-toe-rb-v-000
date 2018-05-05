3e112b9c766be8362c9541f2f7ccc5efd059f5ab
Mohammed Noori <kmanoorib@gmail.com>
2018-05-05 23:55:32 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ end
 
 def valid_move? (board, index)
   
-  if index.between?(0, 8) == TRUE && position_taken?(board, index) != TRUE
+  if index.between?(0, 8) == TRUE && position_taken?(board, index) == false
     TRUE
   else
     FALSE
@@ -50,10 +50,10 @@ def turn (board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
-  if valid_move?(board, index) == FALSE
+  if valid_move?(board, index) == false
     turn (board)
   else
-    move(board, index, "X")
+    move(board, index, current_player(board))
     display_board(board)
   end
 end
@@ -69,3 +69,106 @@ def current_player (board)
     return "O"
   end
 end
+
+def won?(board)
+  empty = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  winning_numbers = [] #store and return this
+
+  WIN_COMBINATIONS.each do |array|
+    if empty == board
+      return false
+    elsif array.all? { |value| board[value] =="X" } || array.all? { |value| board[value] =="O" }
+      winning_numbers = array
+      break
+    else
+      winning_numbers = nil
+    end # end for If statements
+  end # end for enumerations
+  return winning_numbers #return this value
+end #end for function won?
+
+def full?(board)
+ !board.detect { |x| x == " " }
+end
+
+
+=begin
+def draw?(board)
+  if won?(board) #if won, return false
+    return false
+  elsif !won?(board) && !full?(board) #not won AND not full, return false
+    return false
+  elsif !won?(board) && full?(board) #not won AND full, return true
+    return true
+  end
+end
+=end
+
+def draw?(board)
+  if won?(board)
+    return false
+  else
+    if !full?(board)
+      return false
+    else
+      return true
+    end
+  end
+end
+
+
+def over?(board)
+  if draw?(board) || won?(board)
+    return true
+  elsif !won?(board) && !full?(board)
+    return false
+  elsif board.any? { |x| x == " " } && !won?(board)
+    return false
+  end
+end
+
+=begin
+def winner(board)
+  if won?(board)
+    the_winning_letter = won?(board)
+    if the_winning_letter.detect { |value| board[value] == "X" }
+      return "X"
+      elsif the_winning_letter.detect { |value| board[value] =="O" }
+      return "O"
+    end
+  else
+    nil
+end
+=end
+
+def winner(board)
+  winner_return = won?(board)
+  if winner_return == nil
+    return nil
+  else
+    return board[winner_return.first]
+  end
+end
+
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  else
+    puts "Cat's Game!"
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
