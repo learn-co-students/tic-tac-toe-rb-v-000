d1f1d8a4c3af49d836474a4d5f2412b98ffa62c3
Taylor Williams <taylor@thewilliams.net>
2018-05-05 00:30:29 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index >= 0 && index < 9 && !position_taken?(board,index)
+  index.between?(0,8) && !position_taken?(board,index)
 end
 
 def turn(board)
