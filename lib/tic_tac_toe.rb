2cb95136b4674f72038414a0e755d3ed79d95272
Sharmaine Requerme <srqrme@gmail.com>
2018-06-14 18:15:44 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,16 +9,6 @@ WIN_COMBINATIONS = [
   [6, 4, 2]
 ]
 
-def play(board)
-  until over?(board) == true
-    turn(board)
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cat's Game!"
-  end
-end
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -31,69 +21,40 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board, index, character)
-  board[index] = character
-end
-
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
 def turn_count(board)
   turn_counter = 0
   board.each do |index|
     if index == "X" || index == "O"
       turn_counter += 1
     end
-  end
   turn_counter
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def move(board, index, player)
+  board[index] = player
+  board
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def valid_move?(board, index)
+   index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  character = current_player(board)
-  if valid_move?(board, index)
-    move(board, index, character)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-def won?(board)
-  WIN_COMBINATIONS.detect do |win_move|
-    board[win_move[0]] == board[win_move[1]] && board[win_move[1]] == board[win_move[2]] && position_taken?(board, win_move[0])
-  end
-end
-
-def full?(board)
-  board.all?{|character| character == "X" || character == "O"}
-end
-
-def draw?(board)
-  !won?(board) && full?(board)
-end
-
-def over?(board)
-  won?(board) || draw?(board) || full?(board)
-end
-
-def winner(board)
-  if win_move = won?(board)
-    board[win_move[0]]
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+    else
+      turn(board)
+    end
   end
 end
