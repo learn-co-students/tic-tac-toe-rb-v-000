f923460bbfce29ab068eaba97f71bba820f2b626
johnkounelias@gmail.com Kounelias <johnkounelias@gmail.com>
2018-06-19 14:49:41 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,19 @@
 
+WIN_COMBINATIONS = [
+  [0, 1, 2], #top row
+  [3, 4, 5], #middle row
+  [6, 7, 8], #bottom row
+  [0, 3, 6], #left-down row
+  [1, 4, 7], #middle-down row
+  [2, 5, 8], #right-down row
+  [0, 4, 8], #left-diagonal row
+  [2, 4, 6]  #right - diaganal row
+  ]
+  
+  def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
