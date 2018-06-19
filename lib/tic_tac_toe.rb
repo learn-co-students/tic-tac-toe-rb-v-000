9f238f551886d78de958ec3177d2a9b1d5ccf463
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-19 21:59:33 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,45 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8], 
+  [6, 4, 2],
+  [1, 4, 7],
+  [0, 3, 6],
+  [2, 5, 8]
+  ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  separator = "-----------"
+  puts separator
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts separator
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1 
+end
+
+def position_taken?(board, index)
+  if 
+    board[index] == " " || board[index] == """" || board[index] == nil
+    false 
+  else
+    board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+def move(board, index, token)
+  board[index] = token 
+end
+
+def valid_move?(board, index)
+ if index.between?(0, 8) && !position_taken?(board, index) 
+   true
+ else 
+   false 
+  end
+end
