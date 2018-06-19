f1c87bb4134ade2647ec72c25341335ac8091807
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-19 22:18:59 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,3 +43,15 @@ def valid_move?(board, index)
    false 
   end
 end
+
+def turn(board)
+puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
