27fdc526ca8c91bb7f4b9175731967964b5353a0
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-19 22:19:51 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,7 @@ puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, token)
+    move(board, index)
     display_board(board)
   else
     turn(board)
