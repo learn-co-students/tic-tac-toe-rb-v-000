5f06e4b1202a383a48305344fb232cb24f2b8ad5
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-20 03:50:30 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,18 +44,6 @@ def valid_move?(board, index)
   end
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-end
-end
-
 def turn_count(board)
   counter = 0
   board.each do |token|
@@ -74,5 +62,36 @@ def current_player(board)
   end
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  moving = current_player(board)
+  if valid_move?(board, index)
+    move(board, index, moving)
+    display_board(board)
+  else
+    turn(board)
+end
+end
+
+def won?(board)
+    WIN_COMBINATIONS.each do |win_combination| 
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if 
+      position_1 == "X" && position_2 == "X" && position_3 == "X" || 
+      position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    else
+      false
+    end
+  end
 
   
