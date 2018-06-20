cf9e2d3b0c0a0e12771b23f2ad3213c88f0f4c91
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-20 04:05:10 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,10 +89,10 @@ def won?(board)
       position_1 == "X" && position_2 == "X" && position_3 == "X" || 
       position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-      end
-    else
+    else 
       false
-  end
+      end
+    end
 end
 
   
