97f862e2dfede5f6339ed298fee64eefb7c6ca0a
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-23 03:49:26 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,6 @@ def position_taken?(board, index)
     board[index] == " " || board[index] == "" || board[index] == nil
     false 
   else
-    board[index] == "X" || board[index] == "O"
     true
   end
 end
