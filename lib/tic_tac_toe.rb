ccb3fc0f541013fa6fce9cb4feee7c5f9be458c4
Rafael Lopez <lpz.raf@gmail.com>
2018-07-04 23:57:02 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,25 @@
+# WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5,],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
 
+# display_board 
+def display_board(board)
+  puts "   ""|""   ""|""   "
+  puts "-----------"
+  puts "   ""|""   ""|""   "
+  puts "-----------"
+  puts "   ""|""   ""|""   "
+end
+
+board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+
+
+# rspec spec/01_tic_tac_toe_spec.rb
