c585088220d5091af23cd298a87c42d25898142d
Anna Dybas <a.m.dybas@gmail.com>
2018-07-05 19:51:41 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,10 +37,83 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
+  character = current_player(board)
   if valid_move?(board, index)
     move(board, index, character)
     display_board(board)
   else
     turn(board)
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+    count += 1
+    end
+  end
+ count
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else turn_count(board).odd?
+    "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && board[combo[0]] != " "
+  end
+end
+
+def full?(board)
+  board.all? do |letter|
+    if letter == "X" || letter == "O"
+      true
+    else
+      false
+    end
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if draw?(board)|| won?(board) || full?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  status = won?(board)
+  if status.nil?
+    nil
+  else
+    board[status[0]]
+  end
+end
+
+def play(board)
+  num_turns = 0
+  while num_turns < 9 && !over?(board)
+    turn(board)
+    num_turns += 1
+  end
+  if draw?(board)
+    puts "Cat's Game!"
+  else
+    puts "Congratulations #{winner(board)}!"
   end
 end
