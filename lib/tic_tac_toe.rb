30a00baf2bb5657bd20d0760ee951ca64149285d
Rafael Lopez <lpz.raf@gmail.com>
2018-07-05 00:53:21 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,11 +13,11 @@ WIN_COMBINATIONS = [
 # display_board 
 board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 def display_board(board)
-  puts "   ""|""   ""|""   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   ""|""   ""|""   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   ""|""   ""|""   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # input_to_index 
@@ -28,6 +28,39 @@ end
 # move
 def move(board, index, current_player)
    board[index] = current_player
+end
+
+# valid_move? 
+def valid_move?(board, index)
+  if index < 0  || index > 8
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return false
+  else
+    true
+  end
+end
+
+# position_taken? 
+def position_taken?(board, index)
+    if board[index] == " "
+    return false
+    elsif board[index] == "X" || "O" 
+    return true 
+ end
+end
+
+# turn 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board,index)
+  move(board, index)
+  display_board(board)
+  else 
+    turn(board)  
+  end
 end
 
 
