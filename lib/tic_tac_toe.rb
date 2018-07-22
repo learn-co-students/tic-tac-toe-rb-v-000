19d1702a5b2411d1f6d2e1ff383f4a5a4ea87d6f
Heshie Brody <4hbrody@gmail.com>
2018-07-22 22:20:52 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,134 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8], #horizontals
+  [0,3,6],
+  [1,4,7],
+  [2,5,8], #verticals
+  [0,4,8],
+  [2,4,6] #diagonals
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+user_input.to_i - 1
+end
+
+def move(array, index, value)
+  array[index] = value
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+      return true
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  index = input_to_index(input)
+
+  if valid_move?(board, index) == true
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+counter = 0
+board.each do |place|
+  if place == "X" || place == "O"
+    counter += 1
+  end
+end
+  return counter
+end
+
+def current_player(board)
+player = turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+
+  position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combination
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.each do |i|
+    if i == "" || i == " "
+      return false
+    end
+  end
+end
+
+def draw?(board)
+if won?(board) == false
+  if full?(board) == false
+    return false
+  else
+    return true
+    end
+  end
+end
+
+def over?(board)
+  if !won?(board) == false || draw?(board) == true || !full?(board) == false
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+if winner = won?(board) == false
+  return nil
+else
+  win_token = board[won?(board)[0]]
+end
+if win_token == "X"
+  return "X"
+elsif win_token == "O"
+  return "O"
+  end
+end
+
+def play(board)
+if over?(board) == false
+  turn(board)
+  play(board)
+elsif over?(board) == true
+  if !won?(board) == false
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?(board) == true
+      puts "Cat's Game!"
+    end
+  end
+end
