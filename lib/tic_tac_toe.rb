8d1115cf21064f3c56db7933e9c4f92204dd1053
Beatrice Mendoza <mendoza@nyu.edu>
2018-07-22 22:43:26 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,93 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, character)
+  board[index] = character
+end
+
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0, 8)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |board_space|
+    if board_space == "X" || board_space == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combination|
+    if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && position_taken?(board, win_combination[0]) && position_taken?(board, win_combination[1]) && position_taken?(board, win_combination[2])
+      return win_combination
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? {|board_space| board_space == "X" || board_space == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  win_combination = won?(board)
+  win_combination.class == Array ? board[win_combination[0]] : nil
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  won?(board) ? (puts "Congratulations #{winner(board)}!") : (puts "Cat's Game!")
+end
