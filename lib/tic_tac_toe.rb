0acad5af7c92e1fda1d6f25475ee012e4a350ae6
Arielle D <domingu4@gmail.com>
2018-07-27 15:00:55 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,97 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5,],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(display_board, index, character)
+  display_board[index] = character
+end
+
+def position_taken?(board,index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def valid_move?(board,index)
+  if index.between?(0, 8) && board[index] == " " || board[index] == ""
+    true
+  else
+    false
+end
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board,index)
+    move(board,index,current_player(board))
+    display_board(board)
+  else turn(board)
+  end
+end
+
+def turn_count(board)
+  board.count { |turns| turns == "X" || turns == "O"}
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board,combo[0])
+  end
+end
+
+def full?(board)
+  board.all? {|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  if combo = won?(board)
+    board[combo.first]
+  end
+end
+
+def play(board)
+  while !over?(board)
+  turn(board)
+end
+if won?(board)
+  puts "Congratulations #{winner(board)}!"
+else draw?(board)
+  puts "Cat's Game!"
+end
+end
