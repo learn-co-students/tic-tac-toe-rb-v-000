3994e40c06a5c918b188ca7a9974a3195002203b
John Blank <veloblank@gmail.com>
2018-09-13 06:50:36 -0400

    Repeated lab for practice

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -9,8 +11,6 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -19,51 +19,53 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-print display_board(board)
-
-def input_to_index(user_input)
-  user_input.to_i - 1
+def input_to_index(input)
+  input.to_i - 1
 end
 
-def move(board, index, current_player)
-  board[index] = current_player
+def player_move(board, index, marker)
+  board[index] = marker
 end
 
-def position_taken?(board, index)
-  if (board[index] == " " || board[index] == "" || board[index]== nil)
-    return false
-  else
-    return true
-  end
+def position_taken?(board, position)
+  board[position] == "X" || board[position] == "O"
 end
 
 def valid_move?(board, index)
-  if index.between?(1,9)
-    if !position_taken?(board, index)
-      true
-    end
-  end
-  index.between?(0,8) && !position_taken?(board, index)
+  input_to_index(index + 1).between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-      input = gets.strip
-      index = input_to_index(input)
-      if valid_move?(board, index)
-        move(board, index, current_player(board))
-        display_board(board)
-      else
-        turn(board)
-      end
+  input = gets.chomp
+  index = input_to_index(input)
+  if !valid_move?(board, index)
+    turn(board)
+  end
+  if position_taken?(board, index)
+    turn(board)
+  else
+    player_move(board, index, current_player(board))
+  end
+  display_board(board)
 end
 
 def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
+  turn_count = 0
+  board.each do |cell|
+    if cell == "X" || cell == "O"
+      turn_count += 1
+    end
+  end
+  turn_count
 end
 
 def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
 end
 
 def won?(board)
@@ -83,22 +85,11 @@ def draw?(board)
 end
 
 def over?(board)
-  full?(board) || draw?(board) || won?(board)
+  won?(board) || draw?(board) || full?(board)
 end
 
 def winner(board)
-  if takes_the_win = won?(board)
-    board[takes_the_win.first]
-  end
-end
-
-def play(board)
-  while !over?(board)
-    turn(board)
-  end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cats Game!"
+  if player_wins = won?(board)
+    board[player_wins.first]
   end
 end
