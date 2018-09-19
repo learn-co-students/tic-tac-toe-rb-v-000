c43e48570842cc79ec57e049aa0a357686f7227d
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-09-19 01:43:37 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    player_move(board, index, value)
     display_board(board)
   else
     turn(board)
