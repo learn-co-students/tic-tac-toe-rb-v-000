7d07abad15db91fcfe8dcc6a85b54a810a31aab8
Kimberly Aviles <01010110.v@gmail.com>
2018-09-26 12:44:27 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,6 @@
+#board variable with spaces in an array
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 # array containing all possible winning play combinations
 WIN_COMBINATIONS = [
 [0,1,2], #row-wins
@@ -22,8 +25,41 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+#converts user's input into a usable integer
+def input_to_index(input)
+  index = input.to_i - 1
+end
 
-def input_to_index(board, index)
-  user_input = gets.chomp.to_i
-  user_input -= 1
+def player_move(board, index, token)
+  board[index] = token
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0, 8)
+end
+
+def turn(board)
+  puts "Please, select a spot on the board: 1-9"
+  input = gets.strip
+  index = input_to_index(input)
+    if valid_move?(board, index)
+      player_move(board, index, token = "X")
+      display_board(board)
+    else
+      turn(board)
+    end
+end
+
+def turn_count(board)
+  counter = 0
+    board.each do |spot|
+      if spot != " "
+      counter += 1
+      end
+    end
+  return counter
 end
