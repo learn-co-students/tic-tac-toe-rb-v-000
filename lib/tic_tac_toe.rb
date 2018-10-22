633f0b940df0f7267359a259883c86c3473cfd42
Tiffany <ong.tifffany1503@gmail.com>
2018-10-22 21:56:10 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ WIN_COMBINATIONS = [
 def display_board(
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts 'display board'
-#board output
+#BOARD OUTPUT
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
