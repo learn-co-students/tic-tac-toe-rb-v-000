a55b0cf9ac4681b1f848361a5a83139566fd7434
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-10-27 17:11:03 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,88 +1,45 @@
 WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,4,8],
-  [2,4,6],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8]
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+].freeze
 
-def display_board(board)
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-user_input.to_i - 1
-end
-
-def move(board,index,char)
-board[index] = char
-end
-
-def position_taken?(board, index)
-if board[index] == " " || board[index] == "" || board[index] == nil
-  false
-else
-  true
-end
-end
-
-def valid_move?(board,index)
-  if index.between?(0,9) && !position_taken?(board,index)
-    true
-  else
-    false
-end
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-index =  input_to_index(user_input)
-if valid_move?(board,index)
-move(board,index,char = "X")
-display_board(board)
-else
-turn(board)
-end
-end
-
-def turn_count(board)
-  turn = 0
-  board.each do |char|
-    if char == "X" || char == "O"
-      turn +=1
+def play(board)
+  turn(board) until over?(board)
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
   end
 end
-turn
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts '-----------'
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts '-----------'
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def current_player(board)
-if turn_count(board) % 2 == 0
-  "X"
-else
-  "O"
-end
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.any? do |selection|
-  board[selection[0]] == board[selection[1]] &&
-  board[selection[1]] == board[selection[2]] &&
-  position_taken?(board,selection[0])
-end
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+      board[combo[1]] == board[combo[2]] &&
+      position_taken?(board, combo[0])
+  end
 end
 
 def full?(board)
-board.all? do |spot|
-  spot == "X" || spot == "O"
-end
+  board.all? { |token| token == 'X' || token == 'O' }
 end
 
 def draw?(board)
@@ -90,11 +47,43 @@ def draw?(board)
 end
 
 def over?(board)
-  draw?(board) || won?(board)
+  won?(board) || draw?(board)
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def turn(board)
+  puts 'Please enter 1-9:'
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, index)
+  board[index] == 'X' || board[index] == 'O'
+end
+
+def current_player(board)
+  turn_count(board).even? ? 'X' : 'O'
+end
+
+def turn_count(board)
+  board.count { |token| token == 'X' || token == 'O' }
+end
+
+def move(board, index, player)
+  board[index] = player
 end
 
 def winner(board)
   if winning_combo = won?(board)
-    board(winning_combo[0])
-end
+    board[winning_combo.first]
+  end
 end
