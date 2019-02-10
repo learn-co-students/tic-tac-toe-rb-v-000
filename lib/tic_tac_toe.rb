01cba4b9312d0bf11ea0e0aee4e287afa9696427
Michael Ballard <ballard.michael9@gmail.com>
2019-02-10 22:43:19 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,7 +9,7 @@ WIN_COMBINATIONS = [
   [6,4,2]
 ]
 
-def display_board(board)
+def display_board board
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -17,7 +17,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
+def input_to_index user_input
   user_input.to_i - 1
 end
 
@@ -25,17 +25,13 @@ def move board, index, current_player
   board[index] = current_player
 end
 
-def position_taken?(board, index)
+def position_taken? board, index 
   if board[index] == 'X' || board[index] == 'O'
     true
   elsif board[index] == "" || board[index] == " "
     false
   end
 end
-
-# def position_taken?(board, index)
-#   board[index] == 'X' || board[index] == 'O'
-# end
 
 def valid_move? board, index
   index.between?(0,8) && !position_taken?(board, index)
@@ -67,7 +63,7 @@ def move board, position, player
   board[position] = player
 end
 
-def current_player(board)
+def current_player board
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
