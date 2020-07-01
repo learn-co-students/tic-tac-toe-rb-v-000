40f09564347e808a669051172e7b460ef289177f
hkato4188 <hkato@sas.upenn.edu>
2020-07-01 18:02:05 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,46 +1,64 @@
+
 WIN_COMBINATIONS = [
-  [0, 1, 2]
-  [3, 4, 5]
-  [6, 7, 8]
-  [0, 3, 6]
-  [1, 4, 7]
-  [2, 5, 8]
-  [0, 4, 8]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
   [6, 4, 2]
   ]
+
+def play(board)
+  turn(board) until over?(board)
+  won?(board) ? puts("Congratulations #{winner(board)}!") : puts("Cat's Game!")
+end
+
 
 def display_board(board)
  puts " #{board[0]} | #{board[1]} | #{board[2]} "
  puts "-----------"
- puts " #{board[3]} | #{board[4]} | #{board[5]}"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
  puts "-----------"
- puts " #{board[6]} | #{board[7]} | #{board[8]}"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def won?(board)
+  WIN_COMBINATIONS.find do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all?{|position| position == "X" || position == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
 end
 
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, character)
-  board[index] = character
-end
-
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-  end
-end
-
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-  user_input = input_to_index(user_input)
-  if valid_move?(board, user_input)
+  user_position = input_to_index(user_input)
+  if valid_move?(board, user_position)
     character = current_player(board)
-    move(board, user_input, character)
+    move(board, user_position, character)
     display_board(board)
   else
     puts "Invalid Move!"
@@ -48,52 +66,37 @@ def turn(board)
   end
 end
 
+def position_taken?(board, user_position)
+  board[user_position] == "X" || board[user_position] == "O"
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+  # if turn_count(board).even?
+  #   board = "X"
+  # else
+  #   board = "0"
+  # end
+end
+
 def turn_count(board)
-  counter = 0
-  board.each do |spot|
-    counter += 1 if spot != " "
-  end
-  return counter
+  board.count{|position| position == "X" || position == "O"}
+  # counter = 0
+  # board.each do |position|
+  #   counter += 1 if position != " "
+  # end
+  # return counter
+end
+
+def move(board, index, character)
+  board[index] = character
 end
 
 # The #current_player method should take in an argument of the game board and
 # use the #turn_count method to determine if it is "X"'s turn or "O"'s.
 
-def current_player(board)
-  if turn_count(board).even?
-    board = "X"
-  else
-    board = "0"
-  end
-end
-
-def won?(board)
-  WIN_COMBINATIONS.find do |combo|
-    board[combo[0]] == board[cobmo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
-  end
-end
-
-def full?(board)
-  board.all? do |combo|
-    if combo == "X" || combo == "0"
-    end
-  end
-end
-
-def draw?(board)
-  (!won?(board)) && full(board))? true : false
-end
-
-def over?(board)
-  (won?(board) || draw?(board) || full?(board) true : false)
-end
-
 def winner(board)
-  winner_token = won?(board)
-  (winner_token)? board[winner_token[0]] : nil
-end
-
-def play(board)
-  turn(board) until over?(board)
-  won?(board) ? puts("Congratulations #{winner(board)}!") : puts("Cat's Game!")
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
 end
